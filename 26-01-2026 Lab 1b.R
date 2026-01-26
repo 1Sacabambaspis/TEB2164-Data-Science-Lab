5ba4ede91e9ff4ae1fb15626c9cd04267bb10ff9
jiazechong <jiazechong@jiazechongs-MacBook-Air.local>
2026-01-26 11:47:05 +0800

    Initial commit for TEB2164 Data Science Lab

---
+++ 26-01-2026 Lab 1b.R
@@ -0,0 +1,17 @@
+#Task 1
+b<-seq(from=1,to=20)
+print(b^2)
+
+
+#Task 2
+num1=0.956786 
+num2=7.8345901
+print(round(num1,2))
+print(round(num2,3))
+
+#Task 3
+pi= 3.142
+Radius<-readline(prompt="Enter the Radius of the Circle:")
+Radius <- as.numeric(Radius)
+Area = pi*(Radius^2)
+print(round(Area,2))
